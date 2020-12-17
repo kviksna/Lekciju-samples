28e829fc32e3655fde5850ee8ed9b5f95be10edc
kviksna <kviksna@inbox.lv>
2020-12-17 21:03:29 +0200

    print format

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-
 try:
     print(5/0)
 except ZeroDivisionError: print("Error: ZeroDivisionError")
@@ -9,12 +8,45 @@ print(id(res)) #adrese RAM'ā
 
 
 w=5
-h=10
+s=10
 print('w: {}'.format(w))
-salary = 'Salary: {}'.format(salary)
-salary2 = 'Salary: {}{}'.format(salary, 2)
+#salary = 'Salary: {}'.format(salary)
+#salary2 = 'Salary: {}{}'.format(salary, 2)
 
-f'Salary: {}'.format(salary)
+# f'Salary: {}'.format(salary)
 
-#typeof: sub instance
+# typeof: sub instance
+a=5
+b=4
+if a > b and True or True:
+    print("a > b")
+elif a==b:
+    print("a==b")
+else : print("else")
 
+# https://www.programiz.com/python-programming/examples/odd-even
+if 5 % 2 > 0:
+    print("Odd")
+else:
+    print("Even")
+
+
+# https://www.programiz.com/python-programming/examples/leap-year
+
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
